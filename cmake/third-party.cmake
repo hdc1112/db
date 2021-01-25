7d14a0b6e58e6c1d7c16edfa41f33617ba36927d
Dachuan Huang <hdc1112@gmail.com>
2021-01-24 18:27:33 -0800

    add rxcpp

--- cmake/third-party.cmake
+++ cmake/third-party.cmake
@@ -10,6 +10,9 @@ include_directories(${GTEST_INCLUDE_DIRS})
 # git submodule add https://github.com/gabime/spdlog.git external/spdlog
 add_subdirectory(external/spdlog)
 
+# git submodule add https://github.com/ReactiveX/RxCpp.git external/RxCpp
+add_subdirectory(external/RxCpp)
+
 # Export a third-party libraries list
 set(THIRD_PARTY_LIBS)
-list(APPEND THIRD_PARTY_LIBS fmt::fmt ${GTEST_LIBRARIES} spdlog)
+list(APPEND THIRD_PARTY_LIBS fmt::fmt ${GTEST_LIBRARIES} spdlog rxcpp)
